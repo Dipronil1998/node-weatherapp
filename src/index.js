7940b38e952ec6469162fb01fe10074af6c5ef39
Dipronil1998 <dipronildas.net@gmail.com>
2021-12-22 07:18:30 +0530

    heroku setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const hbs = require('hbs');
 const app = express();
 const forecast = require('./utils/forecast');
 const geocode = require('./utils/geocode');
+const port=process.env.PORT || 3000;
 
 const publicDirectoryPath = path.join(__dirname, '../public');
 const viewPath = path.join(__dirname, '../templates/views')
@@ -77,6 +78,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('server is run on 3000');
+app.listen(port, () => {
+    console.log(`server is runnint at ${port}`);
 })
